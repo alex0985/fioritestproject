4816590a98771ab748c21b7f292b83c6ed78074c
Weber Alexander <alexander.weber.it@gmail.com>
2018-05-07 17:54:17 +0000

    Delete JS Lib PDF.js

--- webapp/controller/PdfView.controller.js
+++ webapp/controller/PdfView.controller.js
@@ -1,19 +1,18 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
-	"use strict";
-
-	return Controller.extend("alex.fiori.FioriOnlineTraining.controller.PdfView", {
-
+		"sap/ui/core/mvc/Controller"
+	], function (Controller) {
+		"use strict";
+	return Controller.extend("alex.fiori.FioriOnlineTraining.controller.PdfView", 
+	{
+		onAfterRendering: function() {
+				var url = "";
+				this.getView().byId("idPDFViewer").setSource(url);
+		}
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 		 * @memberOf alex.fiori.FioriOnlineTraining.view.PdfView
 		 */
-			onInit: function() {
-				
-			},
-
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 		 * (NOT before the first rendering! onInit() is used for that one!).
@@ -28,9 +27,6 @@ sap.ui.define([
 		 * This hook is the same one that SAPUI5 controls get after being rendered.
 		 * @memberOf alex.fiori.FioriOnlineTraining.view.PdfView
 		 */
-			onAfterRendering: function() {
-				this.getView().byId("idPDFViewer").setSource("https://www.einfach-fuer-alle.de/download/pdf_barrierefrei.pdf");
-			}
 
 		/**
 		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
@@ -39,7 +35,5 @@ sap.ui.define([
 		//	onExit: function() {
 		//
 		//	}
-
 	});
-
 });
